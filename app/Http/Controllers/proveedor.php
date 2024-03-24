1e4995abc48f1fdb80c6c32a728ad1e6eea521dc
Chochil2004 <chochil.vira@gmail.com>
2024-03-23 20:42:43 -0600

    Commit v1 - Xo - Proveedor

--- app/Http/Controllers/proveedor.php
+++ app/Http/Controllers/proveedor.php
@@ -50,7 +50,7 @@ class proveedor extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Request $request)
     {
         //
     }
