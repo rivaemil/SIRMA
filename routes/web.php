a671fe837e66709069e00e6eda6c3cf875456234
rivaemil <rivasm.emiliano@gmail.com>
2024-04-10 21:49:01 -0600

    VITE, Breeze y npm agregado

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('api/tickets', 'App\Http\Controllers\TicketController@index');
-Route::post('api/tickets', 'App\Http\Controllers\TicketController@store');
-Route::put('api/tickets/{id}', 'App\Http\Controllers\TicketController@update');
-Route::delete('api/tickets/{id}', 'App\Http\Controllers\TicketController@destroy');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('api/materiales', 'App\Http\Controllers\MaterialController@index');
-Route::post('api/materiales', 'App\Http\Controllers\MaterialController@store');
-Route::put('api/materiales/{id}', 'App\Http\Controllers\MaterialController@update');
-Route::delete('api/materiales/{id}', 'App\Http\Controllers\MaterialController@destroy');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::get('api/facturas', 'App\Http\Controllers\FacturaController@index');
-Route::post('api/facturas', 'App\Http\Controllers\FacturaController@store');
-Route::put('api/facturas/{id}', 'App\Http\Controllers\FacturaController@update');
-Route::delete('api/facturas/{id}', 'App\Http\Controllers\FacturaController@destroy');
+require __DIR__.'/auth.php';
